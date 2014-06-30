98f1b5198d306e0787f3f4516a10df64e5f9a3c6
sawan gupta <sawan.gupta@vinsol.com>
2014-06-30 13:22:16 +0530

    count occurence of alphabets in a given string

---
+++ occurence/Bin/main.rb
@@ -0,0 +1,3 @@
+require_relative '../Lib/string.rb'
+puts "Enter a string"
+puts gets().alphabets_hash
---
+++ occurence/Lib/string.rb
@@ -0,0 +1,10 @@
+class String
+  def to_array # breaks a given string into array representing alphabets only
+    self.downcase.scan(/[a-z]/)
+  end
+  def alphabets_hash # returns number of alphabets in a given string
+    number_of_alphabets_in_string = Hash.new(0)
+    self.to_array.each { |alphabet| number_of_alphabets_in_string[alphabet] += 1 }
+    return number_of_alphabets_in_string
+  end
+end
