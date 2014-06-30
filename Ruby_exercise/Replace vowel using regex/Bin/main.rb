5bd7a183a3669c13591f1b57c5d9eb5123a62a5c
sawan gupta <sawan.gupta@vinsol.com>
2014-06-30 14:07:32 +0530

    Replace vowels with * using regex

---
+++ Ruby_exercise/Replace vowel using regex/Bin/main.rb
@@ -0,0 +1,2 @@
+require_relative '../Lib/string.rb'
+puts gets.replace_vowels_with_star
---
+++ Ruby_exercise/Replace vowel using regex/Lib/string.rb
@@ -0,0 +1,5 @@
+class String
+  def replace_vowels_with_star
+    self.gsub(/([aeiou]|[AEIOU])/,'*')
+  end
+end
