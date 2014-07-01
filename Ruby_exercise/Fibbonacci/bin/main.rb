b44b43f1003015f0c3379c172a5180f5b62d5b01
sawan gupta <sawan.gupta@vinsol.com>
2014-07-01 16:49:39 +0530

    spacing and indentation issue fixed

--- Ruby_exercise/Fibbonacci/bin/main.rb
+++ Ruby_exercise/Fibbonacci/bin/main.rb
@@ -1,3 +1,4 @@
 require_relative '../lib/fibonacci.rb'
+
 fibonacci_object = Fibonacci.new
-fibonacci_object.fibonacci_series{ |operand1,operand2| operand1,operand2 = operand2, operand1 + operand2 }
+fibonacci_object.fibonacci_series { |operand1,operand2| operand1,operand2 = operand2, operand1 + operand2 }
