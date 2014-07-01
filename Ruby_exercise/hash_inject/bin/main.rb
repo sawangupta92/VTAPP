6df1bcb5c4e2697a2083967c06c77737e082a711
sawan gupta <sawan.gupta@vinsol.com>
2014-07-01 19:06:55 +0530

    groups the above hash into 2 groups of 'even' and 'odd' length using 'inject'.

---
+++ Ruby_exercise/hash_inject/bin/main.rb
@@ -0,0 +1,2 @@
+require_relative '/home/chirag/Desktop/VT_APP/Ruby_exercise/hash_inject/lib/array.rb'
+['abc','def',1234,'234','abcd','x','mnop','5','zZzZ'].group_array_into_odd_even
---
+++ Ruby_exercise/hash_inject/lib/array.rb
@@ -0,0 +1,27 @@
+class Array
+  
+  def add_into_hash(even,odd)
+    hash_to_check = Hash.new{ |hash, key| hash[key] = [] }
+    hash_to_check["odd"] = odd
+    hash_to_check["even"] =even
+    return hash_to_check
+  end
+  
+  def group_array_into_odd_even
+    even= Hash.new{ |hash, key| hash[key] = [] }
+    odd=Hash.new{ |hash, key| hash[key] = [] }
+   
+    self.each do |word| 
+      word=word.to_s
+      if word.length.even?
+        even[word.length] << word
+      else
+        odd[word.length] << word
+      end
+   
+    end
+    puts add_into_hash(even,odd)
+
+  end
+
+end
