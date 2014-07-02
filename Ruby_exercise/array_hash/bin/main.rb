3b97b8a687dcec210dfe19752603ecc881033d22
sawan <sawan.gupta92@gmail.com>
2014-07-02 16:36:22 +0530

    added Array.flatten to to handle multidimensional arrays

--- Ruby_exercise/array_hash/bin/main.rb
+++ Ruby_exercise/array_hash/bin/main.rb
@@ -1,2 +1,2 @@
 require_relative '../lib/array.rb'
-puts ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'].hash
+puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ'].flatten.group_by_length
--- Ruby_exercise/array_hash/lib/array.rb
+++ Ruby_exercise/array_hash/lib/array.rb
@@ -1,13 +1,12 @@
 class Array
 
-  def hash
-    hash_to_check = Hash.new{ |hash, key| hash[key] = [] }
+  def group_by_length
+    hash_to_check = Hash.new { |hash, key| hash[key] = [] }
 
-    self.each do |word| 
-      word=word.to_s      
-      hash_to_check[word.length] << word
+    each do |word|
+      hash_to_check[word.to_s.length] << word
     end
-    
+
     hash_to_check
   end
 
