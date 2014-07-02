3df0e48e024a6f36fe8460eca282fbec4d09ef90
sawan <sawan.gupta92@gmail.com>
2014-07-02 17:47:21 +0530

    fixed required_relative path

--- Ruby_exercise/hash_inject/bin/main.rb
+++ Ruby_exercise/hash_inject/bin/main.rb
@@ -1,2 +1,2 @@
-require_relative '/home/chirag/Desktop/VT_APP/Ruby_exercise/hash_inject/lib/array.rb'
-puts ['abc','def',1234,'234','abcd','x','mnop','5','zZzZ'].group_array_into_odd_even
+require_relative '../lib/array.rb'
+puts ['abc', 'def', 1234, '234', 'abcd', 'x', 'mnop', '5', 'zZzZ'].group_array_into_odd_even
--- Ruby_exercise/hash_inject/lib/array.rb
+++ Ruby_exercise/hash_inject/lib/array.rb
@@ -1,28 +1,29 @@
 class Array
-  
-  def add_into_hash(even,odd)
-    hash_to_check = Hash.new{ |hash, key| hash[key] = [] }
-    hash_to_check["odd"] = odd
-    hash_to_check["even"] =even
+
+  def add_into_hash(even, odd)
+    hash_to_check = Hash.new { |hash, key| hash[key] = [] }
+    hash_to_check['odd'] = odd
+    hash_to_check['even'] = even
     hash_to_check
   end
-  
+ 
   def group_array_into_odd_even
-    even= Hash.new{ |hash, key| hash[key] = [] }
-    odd=Hash.new{ |hash, key| hash[key] = [] }
-   
-    self.each do |word| 
-      word=word.to_s
+    even= Hash.new { |hash, key| hash[key] = [] }
+    odd=Hash.new { |hash, key| hash[key] = [] }
+
+    each do |word| 
+      word = word.to_s
+
       if word.length.even?
         even[word.length] << word
+
       else
         odd[word.length] << word
       end
-   
+
     end
-    puts add_into_hash(even,odd)
+    add_into_hash(even, odd)
 
   end
 
 end
-['abc','def',1234,'234','abcd','x','mnop','5','zZzZ'].group_array_into_odd_even
