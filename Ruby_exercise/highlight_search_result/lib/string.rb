1faa14dd444deb4205cc0c1d1f02757f35607c95
sawan <sawan.gupta92@gmail.com>
2014-07-04 00:49:31 +0530

    fix case insensative issue

--- Ruby_exercise/highlight_search_result/lib/string.rb
+++ Ruby_exercise/highlight_search_result/lib/string.rb
@@ -1,8 +1,10 @@
 class String
-
   def highlight
+    "("+self+")"
+  end
+  def highlight_count
     regex_to_check = gets.chomp
-    gsub(regex_to_check, "(#{regex_to_check})") + "\n" + count(regex_to_check).to_s
+    gsub(/#{Regexp.quote(regex_to_check)}/i, &:highlight) + "\n" + downcase.count(regex_to_check.downcase).to_s
   end
 
 end
