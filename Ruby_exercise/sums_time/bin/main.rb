db0079b33d651a8e556a6f29d91786738b0ed149
sawan <sawan.gupta92@gmail.com>
2014-07-04 00:55:36 +0530

    fix naming issue

--- Ruby_exercise/sums_time/bin/main.rb
+++ Ruby_exercise/sums_time/bin/main.rb
@@ -1,5 +1,4 @@
 require_relative '../lib/time.rb'
 require 'date'
 
-p q=Time.sum('02:15:06','22:55:06')
-# puts "#{q.hour} #{q.min} #{q.sec}"
+p Time.sum('02:15:06','02:55:06')
--- Ruby_exercise/sums_time/lib/time.rb
+++ Ruby_exercise/sums_time/lib/time.rb
@@ -2,18 +2,20 @@ class Time
 
   Regex_for_time = /^([0-1]\d|[2][0-3])[:][0-5]\d[:][0-5]\d$/
 
-  def self.validate?(string)
-
-    Regex_for_time =~ string
-
+  def self.validate?(time)
+    Regex_for_time =~ time
   end
 
-  def self.sum(string1 = '00:00:00', string2 = '00:00:00')
+  def self.string_to_time(time)
+    DateTime.parse(time + 'IST').to_time
+  end
 
-    if self.validate?(string1) && validate?(string2)
-      string1, string2 = DateTime.parse(string1+'IST').to_time, DateTime.parse(string2+'IST').to_time
-      q=string1+string2.sec+string2.min*60+string2.hour*3600
-      "#{q.day-string1.day} days #{q.hour}:#{q.min}:#{q.sec}"
+  def self.sum(time1 = '00:00:00', time2 = '00:00:00')
+
+    if self.validate?(time1) && validate?(time2)
+      time1, time2 = self.string_to_time(time1), self.string_to_time(time2)
+      q = time1 + time2.sec + time2.min * 60 + time2.hour * 3600
+      "#{q.day-time1.day} days #{q.hour}:#{q.min}:#{q.sec}"
     else
       "not valid"
     end
