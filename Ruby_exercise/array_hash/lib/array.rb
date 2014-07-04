91bd8a7927aae416a78faf111ff7483c90fa7ddb
sawan <sawan.gupta92@gmail.com>
2014-07-04 16:33:55 +0530

    optimizes code uses {} instead of do end in a single line block

--- Ruby_exercise/array_hash/lib/array.rb
+++ Ruby_exercise/array_hash/lib/array.rb
@@ -2,11 +2,7 @@ class Array
 
   def group_by_length
     hash_to_check = Hash.new { |hash, key| hash[key] = [] }
-
-    each do |word|
-      hash_to_check[word.to_s.length] << word
-    end
-
+    each { |word| hash_to_check[word.to_s.length] << word }
     hash_to_check
   end
 
