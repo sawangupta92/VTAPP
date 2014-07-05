dbc012dbc5bbb5b75c9a3cd73b617e4012f0134a
sawan <sawan.gupta92@gmail.com>
2014-07-05 18:52:02 +0530

    removes while loop, adds comments, fixes spacing issue, optimizes code

--- Ruby_exercise/customer/bin/main.rb
+++ Ruby_exercise/customer/bin/main.rb
@@ -3,25 +3,11 @@ require_relative '../lib/customer.rb'
 puts 'enter name to add account'
 
 customer = Customer.new(gets.chomp)
-decision = 'y'
-
-while(!decision.match(/[q]/i))
-  puts 'enter 1 to check account 2 to withdrawal 3 to deposit and 4 to add account q/Q to exit'
-  decision = gets.chomp
-  case decision
-  when '1'
-    puts customer
-  when '2'
-    puts 'enter amount'
-    customer.withdrawal(gets.chomp.to_i) { puts 'you cannot do this' }
-  when '3'
-    puts 'enter amount'
-    customer.deposit(gets.chomp.to_i) { puts 'you can not do this' }
-  when '4'
-    puts 'Enter name of account holder'
-    customer = Customer.new(gets.chomp)
-  when /[q]/i
-  else
-    puts 'try again'
-  end
-end
+puts 'enter 1 to check account 2 to withdrawal 3 to deposit and 4 to add account q/Q to exit'
+puts customer
+puts 'enter amount'
+puts customer.withdrawal(gets.chomp.to_i)
+puts 'enter amount'
+puts customer.deposit(gets.chomp.to_i)
+puts 'Enter name of account holder'
+customer = Customer.new(gets.chomp)
--- Ruby_exercise/customer/lib/customer.rb
+++ Ruby_exercise/customer/lib/customer.rb
@@ -1,6 +1,5 @@
 class Customer
 
-  @account_number = 0
   @@index_of_account_number = 0
   def initialize(name)
     @name = name
@@ -10,22 +9,22 @@ class Customer
   end
 
   def to_s
-    "name is #{@name}, balance is #{@balance}, account number is #{@account_number}"
+    "name is #{ @name }, balance is #{ @balance }, account number is #{ @account_number }"
   end
 
-  def deposit(balance)
+  def deposit(balance) # minimum balance you can add in your account is 1 rupee
     if(balance >= 1)
       @balance += balance
     else
-      yield
+      "you can not do this"
     end
   end
 
-  def withdrawal(balance)
+  def withdrawal(balance) # you can not withdraw more amount than in your account
     if(balance <= @balance)
       @balance -= balance
     else
-      yield
+      "you can not do this"
     end
   end
 
