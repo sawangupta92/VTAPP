4c395e10b77946f7b3393fb7fb8cc3dd14b606f8
sawan <sawan.gupta92@gmail.com>
2014-07-05 19:07:02 +0530

    fixes spacing issue

--- Ruby_exercise/interest_difference/bin/main.rb
+++ Ruby_exercise/interest_difference/bin/main.rb
@@ -2,10 +2,10 @@ require_relative '../lib/interest.rb'
 
 interest=Interest.new.tap do |interest|
   p 'enter principle'
-  interest.principle=gets.chomp.to_i
+  interest.principle = gets.chomp.to_i
   p 'enter time in year'
-  interest.time=gets.chomp.to_i
-  interest.rate=10
+  interest.time = gets.chomp.to_i
+  interest.rate = 10
 end
 
 puts "difference is #{interest.difference}"
