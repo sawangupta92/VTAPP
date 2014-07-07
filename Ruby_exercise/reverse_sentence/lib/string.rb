d271b2a70c80488633ce3e1b5825f910aa9d0625
sawan <sawan.gupta92@gmail.com>
2014-07-07 14:09:25 +0530

    optimizes code

--- Ruby_exercise/reverse_sentence/lib/string.rb
+++ Ruby_exercise/reverse_sentence/lib/string.rb
@@ -1,9 +1,7 @@
 class String
 
-  REGEX = /[\S]+/ # with \w the string 'hi my email is sawan@vinsol.com' will not give expected result
-
   def reverse_arrangement_of_words
-    scan(REGEX).reverse.join(' ')
+    split.reverse.join(' ')
   end
 
 end
