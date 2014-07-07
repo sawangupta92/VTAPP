1efe6c32a47965f2bf5b551930ff1463280a4bcf
sawan <sawan.gupta92@gmail.com>
2014-07-07 13:39:04 +0530

    removes self and optimizes code

--- Ruby_exercise/sums_time/bin/main.rb
+++ Ruby_exercise/sums_time/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/time.rb'
 require 'date'
 
-puts Time.sum('02:20:20','08:20:20')
+puts Time.sum('02:20:20', '08:20:20')
--- Ruby_exercise/sums_time/lib/time.rb
+++ Ruby_exercise/sums_time/lib/time.rb
@@ -13,10 +13,10 @@ class Time
   def self.sum(string1 = '00:00:00', string2 = '00:00:00')
 
     if validate?(string1) && validate?(string2)
-      time1, time2 = self.string_to_time(string1), self.string_to_time(string2)
+      time1, time2 = string_to_time(string1), string_to_time(string2)
       final_time = time1 + time2.sec + time2.min * 60 + time2.hour * 3600
       final_string = "#{ final_time.hour }:" + "#{ final_time.min }:" + "#{ final_time.sec }"
-      if time1.hour+time2.hour > 23
+      if time1.hour + time2.hour > 23
         "1 day #{ final_string }"
       else
         "#{ final_string }"
