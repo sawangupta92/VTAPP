cbd279f66228a3f020a43f57a5f7ae1c0cf82e0b
sawan <sawan.gupta92@gmail.com>
2014-07-09 12:13:17 +0530

    optimize code, enhance readability

--- Ruby_exercise/pascal/bin/main.rb
+++ Ruby_exercise/pascal/bin/main.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/pascal_triangle.rb'
+require_relative '../lib/pascal_triangle'
 
 pascal = Pascal.new
 
--- Ruby_exercise/pascal/lib/pascal_triangle.rb
+++ Ruby_exercise/pascal/lib/pascal_triangle.rb
@@ -1,12 +1,15 @@
-require_relative 'fixnum.rb'
+require_relative 'fixnum'
 
 class Pascal
 
   def series(limit)
-    0.upto(limit) do |value|
-      arr = []
-      0.upto(value) { |number| arr.push(value.factorial / ((number.factorial) * ((value - number).factorial))) }
-      yield arr.join(' ')
+    0.upto(limit-1) do |value|
+      row=''
+      0.upto(value) do |number|
+        result = value.factorial / (number.factorial * (value - number).factorial)
+        row = row + result.to_s + ' '
+      end
+      yield row
     end
   end
 
