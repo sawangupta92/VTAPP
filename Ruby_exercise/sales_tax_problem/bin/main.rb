0f47d26201b016787da62127b6a548ca13acd92b
sawan <sawan.gupta92@gmail.com>
2014-07-09 13:25:42 +0530

    use cart object to add item

--- Ruby_exercise/sales_tax_problem/bin/main.rb
+++ Ruby_exercise/sales_tax_problem/bin/main.rb
@@ -1,27 +1,12 @@
 require_relative '../lib/item.rb'
 
-array_of_objects = []
 cart = ShoppingCart.new()
 
-while true
-  puts 'Name of the product'
-  name_of_product = gets.chomp
-  puts 'enter y if imported else enter any other key'
-  import = gets.chomp == 'y' ? true : false
-  puts 'enter y if Exempt from sales tax else enter any other key'
-  sales_tax_applies = gets.chomp == 'y' ? true : false
-  puts 'enter Price'
-  price = gets.chomp.to_i
-  array_of_objects.push(item = Item.new(name_of_product, import, sales_tax_applies, price))
-  cart.total_price += item.add_tax
-  puts 'enter q to exit else enter any other key'
-  if(gets.chomp == 'q')
-    break
-  end
+cart.add_item do |message|
+  puts message
+  gets.chomp
 end
 
-puts "\n\n"
-array_of_objects.each { |val|
-  puts val.to_s
-}
-puts "\n#{ 'total'.ljust(50, '*') } #{ cart.total_price.to_i } "
+cart.generate_bill do |i|
+  puts i
+end
--- Ruby_exercise/sales_tax_problem/lib/shopping_cart.rb
+++ Ruby_exercise/sales_tax_problem/lib/shopping_cart.rb
@@ -1,8 +1,37 @@
 class ShoppingCart
-  attr_accessor :total_price
+  attr_accessor :total_price, :array_of_objects
 
   def initialize
     @total_price = 0
+  end
+
+  def to_s
+    "\n#{ 'total'.ljust(50, '*') } #{ @total_price } "
+  end
+
+  def add_item
+    @array_of_objects = []
+    while true
+      name_of_product = yield 'Name of the product'
+      import = yield 'enter y if imported else enter any other key'
+      import = import == 'y' ? true : false
+      sales_tax_applies = yield 'enter y if Exempt from sales tax else enter any other key'
+      sales_tax_applies = sales_tax_applies == 'y' ? true : false
+      price = (yield 'enter Price').to_i
+      @array_of_objects.push(item = Item.new(name_of_product, import, sales_tax_applies, price))
+      decision = yield 'enter q to exit else enter any other key'
+      if(decision == 'q')
+        break
+      end
+    end
+  end
+
+  def generate_bill
+    @array_of_objects.each do |item|
+      @total_price += item.add_tax
+      yield item
+    end
+    yield self
   end
 
 end
