4b4010e2b0659f666e40f09167b07f4d441f9883
sawan <sawan.gupta92@gmail.com>
2014-07-10 19:05:26 +0530

    naming issue fix

--- Ruby_exercise/name_raise/lib/name.rb
+++ Ruby_exercise/name_raise/lib/name.rb
@@ -10,7 +10,7 @@ class Name
   def initialize(firstname, lastname)
     raise NullArgument, 'This is not allowed' if valid?(firstname)
     raise NullArgument, 'This is not allowed' if valid?(lastname)
-    raise CapitalizeError, 'first letter of first name is not capital' if firstname[0] != firstname[0].capitalize
+    raise FirstLetterCaseError, 'first letter of first name is not capital' if firstname[0] != firstname[0].capitalize
     @firstname = firstname
     @lastname = lastname
   end
