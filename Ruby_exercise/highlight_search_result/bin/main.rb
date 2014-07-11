219ce0ba9d9508f9b01ab5e6fc7fe85c0e6cdfd4
sawan <sawan.gupta92@gmail.com>
2014-07-11 15:49:20 +0530

    modify user input for regex and convert it to regex

--- Ruby_exercise/highlight_search_result/bin/main.rb
+++ Ruby_exercise/highlight_search_result/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/string.rb'
 
 puts 'Enter string'
-puts gets.chomp.highlight_and_count_search(/(can)/i)
+puts gets.chomp.highlight_and_count_search('can')
--- Ruby_exercise/highlight_search_result/lib/string.rb
+++ Ruby_exercise/highlight_search_result/lib/string.rb
@@ -1,6 +1,8 @@
 class String
-  def highlight_and_count_search(regex)
-    "#{ gsub( regex, '(\1)')}\ntotal occurences found #{scan(regex).length }"
+
+  def highlight_and_count_search(regex_string)
+    regex = Regexp.new("(#{regex_string})",'i')
+    "#{ gsub((regex), '(\\1)')}\ntotal occurences found #{scan(regex).length }"
   end
 
 end
