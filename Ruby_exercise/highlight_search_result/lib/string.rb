e5ac04799ace70770b3762febf3704820c420524
sawan <sawan.gupta92@gmail.com>
2014-07-12 23:35:37 +0530

    spacing issue fix

--- Ruby_exercise/highlight_search_result/lib/string.rb
+++ Ruby_exercise/highlight_search_result/lib/string.rb
@@ -1,8 +1,8 @@
 class String
 
   def highlight_and_count_search(regex_string)
-    regex = Regexp.new("(#{regex_string})",'i')
-    "#{ gsub((regex), '(\\1)')}\ntotal occurences found #{scan(regex).length }"
+    regex = Regexp.new("(#{ regex_string })", 'i')
+    "#{ gsub((regex), '(\\1)') }\nTotal occurences found #{ scan(regex).length }"
   end
 
 end
