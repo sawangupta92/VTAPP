92e991fa5381bff7e6625d04de3d908858208d8d
sawan <sawan.gupta92@gmail.com>
2014-07-23 00:35:10 +0530

    improve names and code readability also use Time class instead of DateTime class

--- Ruby_exercise/sum_time/lib/time.rb
+++ Ruby_exercise/sum_time/lib/time.rb
@@ -1,31 +1,35 @@
-require 'date'
+require 'time'
 
 class Time
   REGEX = /^([0-1]\d|[2][0-3])[:][0-5]\d[:][0-5]\d$/
 
-  def self.sum(*time)
-    if time.all? { |temp_time| valid?(temp_time.to_s) }
-      @temp_total_time = @total_time = DateTime.parse('00:00:00' + 'IST').to_time
-      time.map do |temp_time|
-        temp_time=DateTime.parse(temp_time + 'IST').to_time
-        @total_time += (@temp_total_time - temp_time).abs
+  def self.sum(*string_time)
+    if valid?(*string_time)
+      temp_total_time = total_time = string_to_time('00:00:00')
+      string_time.map do |temp_time|
+        temp_time=string_to_time(temp_time)
+        total_time += (temp_total_time - temp_time).abs
       end
-      @total_time=DateTime.parse('0000-01-01 00:00:00' + 'IST').to_time + (@total_time - Time.now)
-      str=''
-      @total_time.year > 0 ? str.concat("#{@total_time.year} years ") : nil
-      @total_time.month > 1 ? str.concat("#{@total_time.month-1} months ") : nil
-      @total_time.day > 0 ? str.concat("#{@total_time.day} days ") : nil
-      @total_time.hour > 0 ? str.concat("#{@total_time.hour}:") : nil
-      @total_time.min > 0 ? str.concat("#{@total_time.min}:") : nil
-      @total_time.sec > 0 ? str.concat("#{@total_time.sec}") : nil
-      str
+      display(total_time)
     else
       'not valid'
     end
   end
 
-  def self.valid?(string)
-    REGEX =~ string
+  def self.string_to_time(string)
+    Time.parse("0000-01-01 #{string} -00:00")
+  end
+
+  def self.valid?(*strings)
+    strings.all? { |temp_time| REGEX =~ temp_time.to_s }
+  end
+
+  def self.display(total_time)
+    str = ''
+    total_time.year > 0 ? str.concat("#{ total_time.year } years ") : nil
+    total_time.month > 1 ? str.concat("#{ total_time.month-1 } months ") : nil
+    total_time.day > 1 ? str.concat("#{ total_time.day-1 } days ") : nil
+    str.concat("#{ total_time.hour }:").concat("#{ total_time.min }:").concat("#{ total_time.sec }")
   end
 
 end
