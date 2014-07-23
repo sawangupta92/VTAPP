cffafb83be96b9af4d54024a797b279ed4cc5d37
sawan <sawan.gupta92@gmail.com>
2014-07-23 11:50:12 +0530

    fix spacing issue

--- Ruby_exercise/sum_time/lib/time.rb
+++ Ruby_exercise/sum_time/lib/time.rb
@@ -6,8 +6,8 @@ class Time
   def self.sum(*string_time)
     if valid?(*string_time)
       temp_total_time = total_time = string_to_time('00:00:00')
-      string_time.map do |temp_time|
-        temp_time=string_to_time(temp_time)
+      string_time.each do |temp_time|
+        temp_time = string_to_time(temp_time)
         total_time += (temp_total_time - temp_time).abs
       end
       display(total_time)
@@ -16,8 +16,10 @@ class Time
     end
   end
 
+  private
+
   def self.string_to_time(string)
-    Time.parse("0000-01-01 #{string} -00:00")
+    Time.parse("0000-01-01 #{ string } -00:00")
   end
 
   def self.valid?(*strings)
@@ -27,8 +29,8 @@ class Time
   def self.display(total_time)
     str = ''
     total_time.year > 0 ? str.concat("#{ total_time.year } years ") : nil
-    total_time.month > 1 ? str.concat("#{ total_time.month-1 } months ") : nil
-    total_time.day > 1 ? str.concat("#{ total_time.day-1 } days ") : nil
+    total_time.month > 1 ? str.concat("#{ total_time.month - 1 } months ") : nil
+    total_time.day > 1 ? str.concat("#{ total_time.day - 1 } days ") : nil
     str.concat("#{ total_time.hour }:").concat("#{ total_time.min }:").concat("#{ total_time.sec }")
   end
 
