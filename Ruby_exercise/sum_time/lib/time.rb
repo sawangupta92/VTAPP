006e63a74fecc0c50c6322d961a26ef55349eb03
sawan <sawan.gupta92@gmail.com>
2014-07-25 19:11:08 +0530

    improve naming

--- Ruby_exercise/sum_time/lib/time.rb
+++ Ruby_exercise/sum_time/lib/time.rb
@@ -2,13 +2,14 @@ require 'time'
 
 class Time
   REGEX = /^([0-1]\d|[2][0-3])[:][0-5]\d[:][0-5]\d$/
+  REFERENCE_TIME = Time.parse('0000-01-01 00:00:00 -00:00')
 
-  def self.sum(*string_time)
-    if valid?(*string_time)
-      temp_total_time = total_time = string_to_time('00:00:00')
-      string_time.each do |temp_time|
-        temp_time = string_to_time(temp_time)
-        total_time += (temp_total_time - temp_time).abs
+  def self.sum(*stringified_times)
+    if valid?(*stringified_times)
+      total_time = string_to_time('00:00:00')
+      stringified_times.each do |time_string|
+        time = string_to_time(time_string)
+        total_time += (REFERENCE_TIME - time).abs
       end
       display(total_time)
     else
@@ -21,16 +22,16 @@ class Time
     Time.parse("0000-01-01 #{ string } -00:00")
   end
 
-  def self.valid?(*strings)
-    strings.all? { |temp_time| REGEX =~ temp_time.to_s }
+  def self.valid?(*stringified_times)
+    stringified_times.all? { |time_string| REGEX =~ time_string }
   end
 
-  def self.display(total_time)
+  def self.display(time)
     str = ''
-    total_time.year > 0 ? str.concat("#{ total_time.year } years ") : nil
-    total_time.month > 1 ? str.concat("#{ total_time.month - 1 } months ") : nil
-    total_time.day > 1 ? str.concat("#{ total_time.day - 1 } days ") : nil
-    str.concat("#{ total_time.hour }:").concat("#{ total_time.min }:").concat("#{ total_time.sec }")
+    time.year > 0 ? str.concat("#{ time.year } years ") : nil
+    time.month > 1 ? str.concat("#{ time.month - 1 } months ") : nil
+    time.day > 1 ? str.concat("#{ time.day - 1 } days ") : nil
+    str.concat("#{ time.hour }:").concat("#{ time.min }:").concat("#{ time.sec }")
   end
 
 end
