f039e06f06c9ed9af333a6f8d04ed0d1d69be527
sawan <sawan.gupta92@gmail.com>
2014-07-31 00:01:01 +0530

    take firstname and lastName from user and show them

---
+++ js_exercise/input_alert/input_alert.js
@@ -0,0 +1,23 @@
+var Name = function (first_name, last_name) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+}
+Name.prototype = {
+  constructor: Name,
+  validate: function (name) {
+    P = /^([a-z]+)$/i;
+    return P.exec(name);
+  },
+  show: function () {
+    if ((this.validate(this.first_name) && this.validate(this.last_name)) && (this.first_name != null && this.last_name != null)) {
+      alert('Hello ' + this.first_name + ' ' + this.last_name);
+      document.body.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name;
+    }
+  }
+};
+window.onload = function () {
+  first_name = prompt('enter your first name');
+  last_name = prompt('enter your last name');
+  obj = new Name(first_name, last_name);
+  obj.show();
+}
