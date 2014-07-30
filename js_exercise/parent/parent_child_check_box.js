c976cc8e27eb623d773c0eaa9bc8fd6b3767898b
sawan <sawan.gupta92@gmail.com>
2014-07-30 15:45:25 +0530

    implement parent checkbox and their children checkbox

---
+++ js_exercise/parent/parent_child_check_box.js
@@ -0,0 +1,42 @@
+var Check = function (color) {
+  this.outer_checkbox = outer_checkbox;
+  this.mainlist = mainlist;
+};
+
+Check.prototype = {
+  constructor: Check,
+  show: function (outer_list) {
+    outer_list.children[1].style.display = 'block';
+    outer_list.scrollIntoView();
+    for (index = 0; index < outer_list.children[1].childElementCount; index++)
+    {
+      outer_list.children[1].children[index].children[0].checked = false;
+    }
+  },
+  hide: function (outer_list) {
+    outer_list.children[1].style.display = 'none';
+  },
+  show_hide: function (current_outer_checkbox) {
+    outer_list = document.getElementById(current_outer_checkbox.parentNode.attributes['id'].value);
+    if (current_outer_checkbox.checked) {
+      this.show(outer_list)
+    } 
+    else {
+      this.hide(outer_list)
+    }
+  },
+  bind_events: function () {
+    for (index = 0; index < this.outer_checkbox.length; index++) {
+      this.outer_checkbox[index].checked = false;
+      this.outer_checkbox[index].addEventListener('click', this.show_hide.bind(this, this.outer_checkbox[index]))
+      this.show_hide(this.outer_checkbox[index])
+    }
+  },
+};
+
+window.onload = function () {
+  mainlist = document.getElementsByClassName('mainlist');
+  outer_checkbox = document.getElementsByClassName('upper_checkbox');
+  c = new Check(outer_checkbox, mainlist);
+  c.bind_events()
+}
