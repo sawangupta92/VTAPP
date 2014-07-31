c9a866fbe5ca4296d6cf547bca8726b22aa2685d
sawan <sawan.gupta92@gmail.com>
2014-07-31 18:01:15 +0530

    fix naming issue

--- js_exercise/input_url/input_url.js
+++ js_exercise/input_url/input_url.js
@@ -1,8 +1,8 @@
-var NewWindow = function (url) {
+var PopUp = function (url) {
   this.url = url;
 }
-NewWindow.prototype = {
-  constructor: NewWindow,
+PopUp.prototype = {
+  constructor: PopUp,
   show: function () {
     if (this.url.trim() != '' && this.url != null) {
       window.open('http://' + this.url, '_blank', 'width=400, height=450, menubar=no, scrollbar=no, titlebar=no, toolbar=no');
@@ -13,6 +13,6 @@ NewWindow.prototype = {
 };
 window.onload = function () {
   var url = prompt('enter URL');
-  obj = new NewWindow(url);
+  obj = new PopUp(url);
   obj.show();
 }
