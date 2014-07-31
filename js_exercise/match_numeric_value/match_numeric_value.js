9ce9eee2892b8c088d20bd758920b34547babed6
sawan <sawan.gupta92@gmail.com>
2014-07-31 17:17:45 +0530

    fix naming issue

--- js_exercise/match_numeric_value/match_numeric_value.js
+++ js_exercise/match_numeric_value/match_numeric_value.js
@@ -1,18 +1,18 @@
-function MatchNumericalValue(form_elements) {
-  this.form_elements = form_elements;
+function MatchNumericalValue(formElements) {
+  this.formElements = formElements;
 }
 MatchNumericalValue.prototype.REGEX = /^\d+$/;
 MatchNumericalValue.prototype.validate = function () {
-  return this.REGEX.test(this.form_elements[0].value);
+  return this.REGEX.test(this.formElements[0].value);
 }
 MatchNumericalValue.prototype.writeBoolean = function (event) {
-  this.form_elements[1].value = this.validate();
+  this.formElements[1].value = this.validate();
   if (!this.validate()) {
     event.preventDefault();
   }
 }
 MatchNumericalValue.prototype.bindEvents = function () {
-  this.form_elements[2].addEventListener('click', this.writeBoolean.bind(this));
+  this.formElements[2].addEventListener('click', this.writeBoolean.bind(this));
 }
 window.onload = function () {
   var obj = new MatchNumericalValue(document.forms[0]);
