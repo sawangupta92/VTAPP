e4592d7b07eb88a701d9f9718d0692034bfac51b
sawan <sawan.gupta92@gmail.com>
2014-07-31 12:22:51 +0530

    fix naming issue and add case if user enter spaces

--- js_exercise/input_url/input_url.js
+++ js_exercise/input_url/input_url.js
@@ -1,19 +1,18 @@
-var Name = function (url) {
+var NewWindow = function (url) {
   this.url = url;
 }
-Name.prototype = {
-  constructor: Name,
+NewWindow.prototype = {
+  constructor: NewWindow,
   show: function () {
-    if (this.url != '' && this.url != null) {
-      window.open('http://' + this.url, '_blank', 'width=400, height=450 menubar=no scrollbar=no titlebar=no toolbar=no');
-    } 
-    else {
+    if (this.url.trim() != '' && this.url != null) {
+      window.open('http://' + this.url, '_blank', 'width=400, height=450, menubar=no, scrollbar=no, titlebar=no, toolbar=no');
+    } else {
       alert('wrong url');
     }
   }
 };
 window.onload = function () {
   var url = prompt('enter URL');
-  obj = new Name(url);
+  obj = new NewWindow(url);
   obj.show();
 }
