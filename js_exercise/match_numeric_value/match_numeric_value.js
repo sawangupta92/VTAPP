f76c444df8299b0f4fe389924b616435c8ba1dbf
sawan <sawan.gupta92@gmail.com>
2014-07-31 17:08:45 +0530

    fix naming issue, use constant for regex and DRY code

--- js_exercise/match_numeric_value/match_numeric_value.js
+++ js_exercise/match_numeric_value/match_numeric_value.js
@@ -1,24 +1,20 @@
-var MatchNumericalValue = function (my_form) {
-  this.my_form = my_form;
-};
-MatchNumericalValue.prototype = {
-  constructor: MatchNumericalValue,
-  validate: function () {
-    regex = /^\d+$/;
-    return regex.test(this.my_form[0].value);
-  },
-  write_boolean: function (event) {
-    this.my_form[1].value = this.validate();
-    if (this.validate()) {
-    } else {
-      event.preventDefault();
-    }
-  },
-  bind_events: function () {
-    this.my_form[2].addEventListener('click', this.write_boolean.bind(this));
-  },
-};
+function MatchNumericalValue(form_elements) {
+  this.form_elements = form_elements;
+}
+MatchNumericalValue.prototype.REGEX = /^\d+$/;
+MatchNumericalValue.prototype.validate = function () {
+  return this.REGEX.test(this.form_elements[0].value);
+}
+MatchNumericalValue.prototype.writeBoolean = function (event) {
+  this.form_elements[1].value = this.validate();
+  if (!this.validate()) {
+    event.preventDefault();
+  }
+}
+MatchNumericalValue.prototype.bindEvents = function () {
+  this.form_elements[2].addEventListener('click', this.writeBoolean.bind(this));
+}
 window.onload = function () {
-  obj = new MatchNumericalValue(document.forms[0]);
-  obj.bind_events();
+  var obj = new MatchNumericalValue(document.forms[0]);
+  obj.bindEvents();
 }
