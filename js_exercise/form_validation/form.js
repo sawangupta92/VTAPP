13ba70aaaf0d2a874f0c7a6c85905aa654b94e76
sawan <sawan.gupta92@gmail.com>
2014-07-31 19:31:38 +0530

    dry code

--- js_exercise/form_validation/form.js
+++ js_exercise/form_validation/form.js
@@ -1,24 +1,34 @@
 var Form = function (userInput, formElements) {
   this.userInput = userInput;
   this.formElements = formElements;
+  this.flag = 0;
 };
 Form.prototype = {
-  validate: function (event) {
-    for (i = 0; i < this.formElements.length - 3; i++) {
-      if (this.formElements[i].value == '') {
-        event.preventDefault();
-        alert(this.formElements[i].name + ' can\'t be empty');
-      }
+  validateEmpty: function (formElement, event) {
+    if (formElement.value == '') {
+      alert(formElement.name + ' can\'t be empty');
+      this.flag = 1;
     }
-    if (this.formElements['about_me'].value.length < 50) {
-      event.preventDefault();
+  },
+  validateLength: function (formElement, event) {
+    if (formElement.value.length < 50) {
       alert('textarea length must be atleast 50');
+      this.flag = 1;
+    }
+  },
+  validate: function (event) {
+    event.preventDefault();
+    for (i = 0; i < this.formElements.length - 3; i++) {
+      this.validateEmpty(this.formElements[i], event);
+    }
+    this.validateLength(this.formElements['about_me'], event);
+    if(this.flag != 1){
+      this.formElements.submit()
     }
   },
   confirmNotification: function () {
     if (this.formElements['notification'].checked) {
-      con = confirm('you sure you want notifications');
-      if (!con) {
+      if (!confirm('you sure you want notifications')) {
         this.formElements['notification'].checked = false;
       }
     }
