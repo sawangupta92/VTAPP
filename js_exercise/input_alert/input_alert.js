09622137cd194df765f6f15f05631ebb47ff296e
sawan <sawan.gupta92@gmail.com>
2014-07-31 18:12:35 +0530

    fix naming issue

--- js_exercise/input_alert/input_alert.js
+++ js_exercise/input_alert/input_alert.js
@@ -1,23 +1,23 @@
-var Name = function (first_name, last_name) {
-  this.first_name = first_name;
-  this.last_name = last_name;
+var Name = function (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
 }
 Name.prototype = {
   constructor: Name,
   validate: function (name) {
-    var pattern = /^([a-z]+)$/i;
-    return pattern.exec(name);
+    var PATTERN = /^([a-z]+)$/i;
+    return PATTERN.exec(name);
   },
   show: function () {
-    if ((this.validate(this.first_name) && this.validate(this.last_name)) && (this.first_name != null && this.last_name != null)) {
-      alert('Hello ' + this.first_name + ' ' + this.last_name);
-      document.body.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name;
+    if ((this.validate(this.firstName) && this.validate(this.lastName)) && (this.firstName != null && this.lastName != null)) {
+      alert('Hello ' + this.firstName + ' ' + this.lastName);
+      document.body.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName;
     }
   }
 };
 window.onload = function () {
-  first_name = prompt('enter your first name');
-  last_name = prompt('enter your last name');
-  obj = new Name(first_name, last_name);
+  firstName = prompt('enter your first name');
+  lastName = prompt('enter your last name');
+  obj = new Name(firstName, lastName);
   obj.show();
 }
