451c1664d1132892d7293255a2d2530f63f44ec5
sawan <sawan.gupta92@gmail.com>
2014-08-01 16:43:24 +0530

    add case if user enter wrong name prompt will again reappear

--- js_exercise/input_alert/input_alert.js
+++ js_exercise/input_alert/input_alert.js
@@ -1,23 +1,30 @@
 var Name = function (firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+  this.firstName = null;
+  this.lastName = null;
 }
 Name.prototype = {
   constructor: Name,
   validate: function (name) {
     var PATTERN = /^([a-z]+)$/i;
-    return PATTERN.exec(name);
+    return PATTERN.test(name) && name;
   },
   show: function () {
     if ((this.validate(this.firstName) && this.validate(this.lastName)) && (this.firstName != null && this.lastName != null)) {
       alert('Hello ' + this.firstName + ' ' + this.lastName);
       document.body.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName;
     }
+  },
+  input: function (msg){
+    var name = '';
+    while(!this.validate(name)){
+      name = prompt(msg);
+    }
+    return name;
   }
 };
 window.onload = function () {
-  firstName = prompt('enter your first name');
-  lastName = prompt('enter your last name');
-  obj = new Name(firstName, lastName);
+  obj = new Name();
+  obj.firstName = obj.input('enter your first name');
+  obj.lastName = obj.input('enter your last name');
   obj.show();
 }
