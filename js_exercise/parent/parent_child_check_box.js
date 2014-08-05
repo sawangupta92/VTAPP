80c89460350dbbd460586927a75c94e4a20c3bf7
sawan <sawan.gupta92@gmail.com>
2014-08-05 15:37:59 +0530

    decrease dom dependency and deselect at hide

--- js_exercise/parent/parent_child_check_box.js
+++ js_exercise/parent/parent_child_check_box.js
@@ -1,41 +1,45 @@
-var Check = function (outerCheckbox, mainList) {
+var Check = function (outerCheckbox, mainList, innerList) {
   this.outerCheckbox = outerCheckbox;
   this.mainList = mainList;
+  this.innerList = innerList;
 };
-
 Check.prototype = {
   constructor: Check,
   show: function (outerList) {
     outerList.children[1].style.display = 'block';
     outerList.scrollIntoView();
-    for (index = 0; index < outerList.children[1].childElementCount; index++)
+  },
+  uncheckList: function (currentInnerCheckbox) {
+    for (var index = 0; index < currentInnerCheckbox.childElementCount; index++)
     {
-      outerList.children[1].children[index].children[0].checked = false;
+      currentInnerCheckbox.children[index].children[0].checked = false;
     }
   },
-  hide: function (outerList) {
+  hide: function (outerList, currentInnerCheckbox) {
     outerList.children[1].style.display = 'none';
+    this.uncheckList(currentInnerCheckbox);
   },
-  showHide: function (current_outer_checkbox) {
-    outerList = document.getElementById(current_outer_checkbox.parentNode.attributes['id'].value);
-    if (current_outer_checkbox.checked) {
+  showHide: function (currentOuterCheckbox, currentInnerCheckbox) {
+    outerList = document.getElementById(currentOuterCheckbox.parentNode.attributes['id'].value);
+    if (currentOuterCheckbox.checked) {
       this.show(outerList)
     } else {
-      this.hide(outerList)
+      this.hide(outerList, currentInnerCheckbox)
     }
   },
   bindEvents: function () {
-    for (index = 0; index < this.outerCheckbox.length; index++) {
-      this.outerCheckbox[index].checked = false;
-      this.outerCheckbox[index].addEventListener('click', this.showHide.bind(this, this.outerCheckbox[index]))
-      this.showHide(this.outerCheckbox[index])
+    for (var index = 0; index < this.outerCheckbox.length; index++) {
+      this.outerCheckbox[index].addEventListener('click', this.showHide.bind(this, this.outerCheckbox[index], this.innerList[index]));
     }
   },
 };
-
 window.onload = function () {
   var mainList = document.getElementsByClassName('mainlist'),
-      outerCheckbox = document.getElementsByClassName('upper_checkbox');
-  obj = new Check(outerCheckbox, mainList);
+      outerCheckbox = document.getElementsByClassName('upper_checkbox'),
+      innerList = document.getElementsByClassName('innerList');
+  obj = new Check(outerCheckbox, mainList, innerList);
   obj.bindEvents()
+ for (var index = 0; index < innerList.length; index++) {
+   obj.showHide(outerCheckbox[index], innerList[index])
+  }
 }
