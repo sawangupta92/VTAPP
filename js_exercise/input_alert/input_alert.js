4c6a1be99c8e51cf90a0364e333ebd3eef520b8c
sawan <sawan.gupta92@gmail.com>
2014-08-07 19:21:51 +0530

    optimize code

--- js_exercise/input_alert/input_alert.js
+++ js_exercise/input_alert/input_alert.js
@@ -1,30 +1,26 @@
 var Name = function (firstName, lastName) {
-  this.firstName = null;
-  this.lastName = null;
+  this.firstName = this.inputName('enter your first name');
+  this.lastName = this.inputName('enter your last name');
 }
-Name.prototype = {
-  constructor: Name,
-  validate: function (name) {
-    var PATTERN = /^([a-z]+)$/i;
-    return PATTERN.test(name) && name;
-  },
-  show: function () {
-    if ((this.validate(this.firstName) && this.validate(this.lastName)) && (this.firstName != null && this.lastName != null)) {
-      alert('Hello ' + this.firstName + ' ' + this.lastName);
-      document.body.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName;
-    }
-  },
-  input: function (msg){
-    var name = '';
-    while(!this.validate(name)){
-      name = prompt(msg);
-    }
-    return name;
+var REGEX = {
+  name: /^([a-z]+)$/i,
+};
+Name.prototype.validate = function (name) {
+  return REGEX.name.test(name);
+};
+Name.prototype.display = function () {
+  alert('Hello ' + this.firstName + ' ' + this.lastName);
+  document.body.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName;
+};
+Name.prototype.inputName = function (msg){
+  name = prompt(msg);
+  if(!this.validate(name) | name == 'null'){
+    alert('please enter valid name consisting alphabets only')
+    this.inputName(msg)
   }
+  return name;
 };
 window.onload = function () {
   obj = new Name();
-  obj.firstName = obj.input('enter your first name');
-  obj.lastName = obj.input('enter your last name');
-  obj.show();
+  obj.display();
 }
