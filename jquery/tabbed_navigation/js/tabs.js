4f01eab0b9617212868759f7859e419b66ad915e
sawan <sawan.gupta92@gmail.com>
2014-08-07 12:35:58 +0530

    optimize code

--- jquery/tabbed_navigation/js/tabs.js
+++ jquery/tabbed_navigation/js/tabs.js
@@ -1,35 +1,40 @@
-//modules.each()
-var navigation = function(modules){
-  _that = this;
-  this.modules = modules;
-  this.list;
-}
-navigation.prototype.addList = function(){
-  var list = '<ul class=nav></ul>';
-  _that.list = $(list)
-  _that.modules.first().before(list);
-  // console.log(_that.modules)
-  _that.modules.each(function(index, elem){
- //   console.log($(elem))
-    $('.nav').append('<li>'+$('#' + this.id).find('h2').html()+'</li>')
+var navigation = function (allModules) {
+  this.allModules = allModules;
+};
+
+navigation.prototype.hideAll = function () {
+  this.allModules.hide();
+};
+
+navigation.prototype.createList = function () {
+  $('.module:first').before('<li class=nav></li>');
+};
+
+navigation.prototype.addElementsInList = function () {
+  this.allModules.each(function (index, elem) {
+    $('.nav').append('<li>' + $('#' + this.id).find('h2').html() + '</li>');
   })
 };
-navigation.prototype.test = function(){
-  console.log(this)
-//  this.find('li').hide()
-}
-navigation.prototype.bindEvents = function(){
-//  alert(_that.list)
-  _that.list.each(function(index, elem){
-    console.log($('.nav').find('li'))
-    $('.nav').find('li').bind('click',_that.test)
-//    console.log($('#' + this.class))
-    //$('#' + this.id).bind('click', _that.test)
+
+navigation.prototype.display = function (element) {
+  $('#' + $(element).addClass('current').html().toLowerCase()).show();
+  this.allModules.not($('#' + $(element).html().toLowerCase())).hide();
+  $('.nav li').not(element).removeClass('current');
+};
+
+navigation.prototype.bindEvents = function () {
+  var _this = this;
+  $('.nav li').on('click', function(){
+    _this.display(this);
   })
+};
+
+navigation.prototype.initialize = function(){
+  this.createList();
+  this.addElementsInList();
+  this.hideAll();
+  this.bindEvents();  
 }
-var modules = $('div.module');
-obj = new navigation(modules);
-obj.addList();
-//$('.nav').find('li')
-obj.bindEvents();
-//$('div.module').find('li')
+
+var obj = new navigation($('.module'));
+obj.initialize();
