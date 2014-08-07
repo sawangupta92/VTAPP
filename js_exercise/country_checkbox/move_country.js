9213cab39dc0d4f01a7f2b1712e6f3c30ccd94c3
sawan <sawan.gupta92@gmail.com>
2014-08-07 19:21:05 +0530

    optimize code and use hash

--- js_exercise/country_checkbox/move_country.js
+++ js_exercise/country_checkbox/move_country.js
@@ -1,22 +1,25 @@
-var Country = function (add, remove, firstSelectBox, secondSelectBox) {
-  this.add = add;
-  this.remove = remove;
-  this.firstSelectBox = firstSelectBox;
-  this.secondSelectBox = secondSelectBox;
+var Country = function (elements) {
+  this.add = elements.add;
+  this.remove = elements.remove;
+  this.firstSelectBox = elements.firstSelectBox;
+  this.secondSelectBox = elements.secondSelectBox;
 };
-Country.prototype = {
-  constructor: Country,
-  move: function (firstSelectBox, secondSelectBox) {
-    for (index = 0; index < firstSelectBox.selectedOptions.length; ) {
-      secondSelectBox.appendChild(firstSelectBox.selectedOptions[index]);
-    }
-  },
-  bindEvents: function () {
-    this.add.addEventListener('click', this.move.bind(this, this.firstSelectBox, this.secondSelectBox));
-    this.remove.addEventListener('click', this.move.bind(this, this.secondSelectBox, this.firstSelectBox));
+Country.prototype.move = function (firstSelectBox, secondSelectBox) {
+  for (index = 0; index < firstSelectBox.selectedOptions.length; ) {
+    secondSelectBox.appendChild(firstSelectBox.selectedOptions[index]);
   }
+};
+Country.prototype.bindEvents = function () {
+  this.add.addEventListener('click', this.move.bind(this, this.firstSelectBox, this.secondSelectBox));
+  this.remove.addEventListener('click', this.move.bind(this, this.secondSelectBox, this.firstSelectBox));
 }
 window.onload = function () {
-  object = new Country(document.getElementById('add'), document.getElementById('remove'), document.getElementById('first_select_box'), document.getElementById('second_select_box'));
+  var elements = {
+    'add': document.getElementById('add'),
+    'remove': document.getElementById('remove'),
+    'firstSelectBox': document.getElementById('first_select_box'),
+    'secondSelectBox': document.getElementById('second_select_box')
+  }
+  object = new Country(elements);
   object.bindEvents();
 }
